daf471511043f5bd3efedce86421aa2f3c491395
Julianne Quinn <julianneq@users.noreply.github.com>
2020-09-03 17:35:20 -0400

    Update verifyGenerator.py

--- verifyGenerator.py
+++ verifyGenerator.py
@@ -23,42 +23,53 @@ def getWeather(data, database, row):
     if database[row,3] == 2 and database[row,4] == 29:
         database[row,4] = 28
 
-    lastPlantDate = np.where(dates==datetime(2010,int(database[row,3]),int(database[row,4])))[0][0]
-    remainingDays = len(dates) - lastPlantDate + 1
-    if remainingDays < 30:
-        month1 = np.zeros([30,2])
-        month2 = np.zeros([30,2])
-        month3 = np.zeros([30,2])
-    elif remainingDays >= 30 and remainingDays < 60:
+    lastPlantDate = np.where(dates==datetime(2010,int(database[row,3]),int(database[row,4])))[0][0] # planting date in last year
+    remainingDays = len(dates) - lastPlantDate + 1 # number of days left in the calendar year
+    # initialize all months with 30 years; add an extra year if there are enough remaining days in 2010 for a full month
+    month1 = np.zeros([30,2])
+    month2 = np.zeros([30,2])
+    month3 = np.zeros([30,2])
+    month4 = np.zeros([30,2])
+    month5 = np.zeros([30,2])
+    if remainingDays >= 30: # 1st month has an extra year of data
         month1 = np.zeros([31,2])
-        month2 = np.zeros([30,2])
-        month3 = np.zeros([30,2])
-    elif remainingDays >= 60 and remainingDays < 90:
-        month1 = np.zeros([31,2])
+    elif remainingDays >= 60: # 1st 2 months have an extra year of data
         month2 = np.zeros([31,2])
-        month3 = np.zeros([30,2])
-    else:
-        month1 = np.zeros([31,2])
-        month2 = np.zeros([31,2])
+    elif remainingDays >= 90: # 1st 3 months have an extra year of data
         month3 = np.zeros([31,2])
+    elif remainingDays >= 120: # 1st 4 months have an extra year of data
+        month4 = np.zeros([31,2])
+    elif remainingDays >= 150: #all months have an extra year of data
+        month5 = np.zeros([31,2])
         
     # populate monthly data matrices
-    for i in range(np.shape(month1)[0]):
-        day1 = np.where(dates==datetime(1980+i,int(database[row,3]),int(database[row,4])))[0][0]
-        month1[i,0] = np.mean(data[day1:(day1+30),0])
-        month1[i,1] = np.sum(data[day1:(day1+30),1])
-        
-    for i in range(np.shape(month2)[0]):
-        day1 = np.where(dates==datetime(1980+i,int(database[row,3]),int(database[row,4])))[0][0]
-        month2[i,0] = np.mean(data[(day1+30):(day1+60),0])
-        month2[i,1] = np.sum(data[(day1+30):(day1+60),1])
-        
-    for i in range(np.shape(month3)[0]):
-        day1 = np.where(dates==datetime(1980+i,int(database[row,3]),int(database[row,4])))[0][0]
-        month3[i,0] = np.mean(data[(day1+60):(day1+90),0])
-        month3[i,1] = np.sum(data[(day1+60):(day1+90),1])
+    months = [month1, month2, month3, month4, month5]
+    nMonths = len(months)
+    for k in range(nMonths): # loop through 5 months
+        for i in range(np.shape(months[k])[0]): # loop through 30 or 31 years
+            day1 = np.where(dates==datetime(1980+i,int(database[row,3]),int(database[row,4])))[0][0] # planting date in year i
+            months[k][i,0] = np.mean(data[(day1+30*k):(day1+30*(k+1)),0]) # mean temperature
+            months[k][i,1] = np.sum(data[(day1+30*k):(day1+30*(k+1)),1]) # total precipitation
             
-    return month1, month2, month3
+    return months
+
+def makeTempSubplot(ax, months, tempData, monthNo):
+    ax.hist(months[monthNo-1][:,0], color='b', alpha=0.5, density=True)
+    ax.hist(tempData[:,monthNo-1], color='g', alpha=0.5, density=True)
+    ax.set_title('Temperature Month ' + str(monthNo))
+    
+    return None
+    
+def makePrecipSubplot(ax, months, precipData, monthNo, label=False):
+    if label == False:
+        ax.hist(months[monthNo-1][:,1], color='b', alpha=0.5, density=True)
+        ax.hist(precipData[:,monthNo-1], color='g', alpha=0.5, density=True)
+    else:
+        ax.hist(months[monthNo-1][:,1], color='b', alpha=0.5, density=True, label='Historical')
+        ax.hist(precipData[:,monthNo-1], color='g', alpha=0.5, density=True, label='Synthetic')        
+    ax.set_title('Precipitation Month ' + str(monthNo))
+    
+    return None
         
 
 for file in fileList:
@@ -72,45 +83,26 @@ for file in fileList:
         row = np.intersect1d(np.where(database[:,1] == lat)[0], np.where(database[:,2] == long)[0])
         
         if len(row) > 0:
-            obsMonth1, obsMonth2, obsMonth3 = getWeather(obsWeather, database, row)
+            obsMonths = getWeather(obsWeather, database, row)
             simPrecip = np.loadtxt('Weather/synthetic/' + file[0:-4] + '_precip.txt')
             simTemp = np.loadtxt('Weather/synthetic/' + file[0:-4] + '_temp.txt')
             
             fig = plt.figure()
-            ax = fig.add_subplot(3,2,1) # temp on left, precip on right; rows = months
-            ax.hist(obsMonth1[:,0], color='b', alpha=0.5, density=True)
-            ax.hist(simTemp[:,0], color='g', alpha=0.5, density=True)
-            ax.set_title('Temperature Month 1')
-            
-            ax = fig.add_subplot(3,2,2) # temp on left, precip on right; rows = months
-            ax.hist(obsMonth1[:,1], color='b', alpha=0.5, density=True)
-            ax.hist(simPrecip[:,0], color='g', alpha=0.5, density=True)
-            ax.set_title('Precipitation Month 1')
-            
-            ax = fig.add_subplot(3,2,3) # temp on left, precip on right; rows = months
-            ax.hist(obsMonth2[:,0], color='b', alpha=0.5, density=True)
-            ax.hist(simTemp[:,1], color='g', alpha=0.5, density=True)
-            ax.set_title('Temperature Month 2')
-            
-            ax = fig.add_subplot(3,2,4) # temp on left, precip on right; rows = months
-            ax.hist(obsMonth2[:,1], color='b', alpha=0.5, density=True)
-            ax.hist(simPrecip[:,1], color='g', alpha=0.5, density=True)
-            ax.set_title('Precipitation Month 2')
-            
-            ax = fig.add_subplot(3,2,5) # temp on left, precip on right; rows = months
-            ax.hist(obsMonth3[:,0], color='b', alpha=0.5, density=True)
-            ax.hist(simTemp[:,2], color='g', alpha=0.5, density=True)
-            ax.set_title('Temperature Month 3')
-            
-            ax = fig.add_subplot(3,2,6) # temp on left, precip on right; rows = months
-            ax.hist(obsMonth3[:,1], color='b', alpha=0.5, density=True, label='Historical')
-            ax.hist(simPrecip[:,2], color='g', alpha=0.5, density=True, label='Synthetic')
-            ax.set_title('Precipitation Month 3')
+    
+            for i in range(len(obsMonths)):
+                ax = fig.add_subplot(2,5,i+1) # temp on top, precip on bottom; columns = months
+                makeTempSubplot(ax, obsMonths, simTemp, i+1)
+                
+                ax = fig.add_subplot(2,5,i+6)
+                if i != len(obsMonths)-1:
+                    makePrecipSubplot(ax, obsMonths, simPrecip, i+1)
+                else:
+                    makePrecipSubplot(ax, obsMonths, simPrecip, i+1, True)
             
             handles, labels = plt.gca().get_legend_handles_labels()
-            fig.subplots_adjust(bottom=0.15)
+            fig.subplots_adjust(bottom=0.1)
             fig.legend(handles, labels, loc='lower center',ncol=2,fontsize=16,frameon=True)
-            fig.set_size_inches([10.5,11.5])
+            fig.set_size_inches([19.2,9.6])
             fig.savefig('Weather/validation/' + file[0:-4] + '.png')
             fig.clf()
         
